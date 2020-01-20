1e208507b68c2d5c12bff03dc82c87f58cfa1929
Vladimir Ch <chvv.spb@gmail.com>
2020-01-20 10:05:37 +0300

    Sources version 1

---
+++ Lab3/AVL.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Text;
+
+namespace Lab3
+{
+    public class AVL
+    {
+        Node root;
+        public AVL()
+        {
+        }
+        public void Add(int data)
+        {
+            Node newItem = new Node(data);
+            if (root == null)
+            {
+                root = newItem;
+            }
+            else
+            {
+                root = RecursiveInsert(root, newItem);
+            }
+        }
+        private Node RecursiveInsert(Node current, Node n)
+        {
+            if (current == null)
+            {
+                current = n;
+                return current;
+            }
+            else if (n.data < current.data)
+            {
+                current.left = RecursiveInsert(current.left, n);
+                current = balance_tree(current);
+            }
+            else if (n.data > current.data)
+            {
+                current.right = RecursiveInsert(current.right, n);
+                current = balance_tree(current);
+            }
+            return current;
+        }
+        private Node balance_tree(Node current)
+        {
+            int b_factor = balance_factor(current);
+            if (b_factor > 1)
+            {
+                if (balance_factor(current.left) > 0)
+                {
+                    current = RotateLL(current);
+                }
+                else
+                {
+                    current = RotateLR(current);
+                }
+            }
+            else if (b_factor < -1)
+            {
+                if (balance_factor(current.right) > 0)
+                {
+                    current = RotateRL(current);
+                }
+                else
+                {
+                    current = RotateRR(current);
+                }
+            }
+            return current;
+        }
+        public void Delete(int target)
+        {//and here
+            root = Delete(root, target);
+        }
+        private Node Delete(Node current, int target)
+        {
+            Node parent;
+            if (current == null)
+            { return null; }
+            else
+            {
+                //left subtree
+                if (target < current.data)
+                {
+                    current.left = Delete(current.left, target);
+                    if (balance_factor(current) == -2)//here
+                    {
+                        if (balance_factor(current.right) <= 0)
+                        {
+                            current = RotateRR(current);
+                        }
+                        else
+                        {
+                            current = RotateRL(current);
+                        }
+                    }
+                }
+                //right subtree
+                else if (target > current.data)
+                {
+                    current.right = Delete(current.right, target);
+                    if (balance_factor(current) == 2)
+                    {
+                        if (balance_factor(current.left) >= 0)
+                        {
+                            current = RotateLL(current);
+                        }
+                        else
+                        {
+                            current = RotateLR(current);
+                        }
+                    }
+                }
+                //if target is found
+                else
+                {
+                    if (current.right != null)
+                    {
+                        //delete its inorder successor
+                        parent = current.right;
+                        while (parent.left != null)
+                        {
+                            parent = parent.left;
+                        }
+                        current.data = parent.data;
+                        current.right = Delete(current.right, parent.data);
+                        if (balance_factor(current) == 2)//rebalancing
+                        {
+                            if (balance_factor(current.left) >= 0)
+                            {
+                                current = RotateLL(current);
+                            }
+                            else { current = RotateLR(current); }
+                        }
+                    }
+                    else
+                    {   //if current.left != null
+                        return current.left;
+                    }
+                }
+            }
+            return current;
+        }
+        public string Find(int key)
+        {
+            Node found = Find(key, root);
+            if (found != null && found.data == key)
+            {
+                return String.Format("({0})", key);
+            }
+            return String.Empty;
+        }
+        private Node Find(int target, Node current)
+        {
+            if (target == current.data) return current;
+            if (target < current.data)
+            {
+                if (current.left != null) return Find(target, current.left);
+            }
+            else
+            {
+                if (current.right != null) return Find(target, current.right);
+            }
+            return null;
+        }
+        public string PrintTree()
+        {
+            if (root == null)
+            {
+                return String.Empty;
+            }
+            return InOrderPrintTree(root);
+        }
+        private string InOrderPrintTree(Node current)
+        {
+            if (current == null) return String.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(InOrderPrintTree(current.left));
+            sb.Append(String.Format("({0})", current.data));
+            sb.Append(InOrderPrintTree(current.right));
+            return sb.ToString();
+        }
+        private int max(int l, int r)
+        {
+            return l > r ? l : r;
+        }
+        private int getHeight(Node current)
+        {
+            int height = 0;
+            if (current != null)
+            {
+                int l = getHeight(current.left);
+                int r = getHeight(current.right);
+                int m = max(l, r);
+                height = m + 1;
+            }
+            return height;
+        }
+        private int balance_factor(Node current)
+        {
+            int l = getHeight(current.left);
+            int r = getHeight(current.right);
+            int b_factor = l - r;
+            return b_factor;
+        }
+        private Node RotateRR(Node parent)
+        {
+            Node pivot = parent.right;
+            parent.right = pivot.left;
+            pivot.left = parent;
+            return pivot;
+        }
+        private Node RotateLL(Node parent)
+        {
+            Node pivot = parent.left;
+            parent.left = pivot.right;
+            pivot.right = parent;
+            return pivot;
+        }
+        private Node RotateLR(Node parent)
+        {
+            Node pivot = parent.left;
+            parent.left = RotateRR(pivot);
+            return RotateLL(parent);
+        }
+        private Node RotateRL(Node parent)
+        {
+            Node pivot = parent.right;
+            parent.right = RotateLL(pivot);
+            return RotateRR(parent);
+        }
+    }
+}
---
+++ Lab3Tests/AVLTests.cs
@@ -0,0 +1,263 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Tests
+{
+    [TestClass()]
+    public class AVLTests
+    {
+        [TestMethod()]
+        public void Test1()
+        {
+            AVL avl = new AVL();
+            Assert.AreEqual(String.Empty, avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test2()
+        {
+            AVL avl = new AVL();
+            avl.Add(5);
+            Assert.AreEqual("(5)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test3()
+        {
+            // Малое правое вращение
+            AVL avl = new AVL();
+            avl.Add(5);
+            avl.Add(3);
+            avl.Add(4);
+            Assert.AreEqual("(3)(4)(5)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test4()
+        {
+            // Малое левое вращение
+            AVL avl = new AVL();
+            avl.Add(3);
+            avl.Add(5);
+            avl.Add(4);
+            Assert.AreEqual("(3)(4)(5)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test5()
+        {
+            // Большое левое вращение
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(5);
+            avl.Add(4);
+            avl.Add(6);
+            avl.Add(3);
+            Assert.AreEqual("(1)(2)(3)(4)(5)(6)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test6()
+        {
+            // Большое правое вращение
+            AVL avl = new AVL();
+            avl.Add(5);
+            avl.Add(6);
+            avl.Add(3);
+            avl.Add(4);
+            avl.Add(2);
+            avl.Add(1);
+            Assert.AreEqual("(1)(2)(3)(4)(5)(6)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test7()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(3);
+            avl.Delete(2);
+            Assert.AreEqual("(1)(3)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test8()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(3);
+            avl.Delete(1);
+            Assert.AreEqual("(2)(3)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test9()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(3);
+            avl.Delete(3);
+            Assert.AreEqual("(1)(2)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test10()
+        {
+            AVL avl = new AVL();
+            avl.Delete(1);
+            Assert.AreEqual(String.Empty, avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test11()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(3);
+            Assert.AreEqual("(2)", avl.Find(2));
+        }
+        [TestMethod()]
+        public void Test12()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(3);
+            Assert.AreEqual("(1)", avl.Find(1));
+        }
+        [TestMethod()]
+        public void Test13()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(3);
+            Assert.AreEqual("(3)", avl.Find(3));
+        }
+        [TestMethod()]
+        public void Test14()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(3);
+            Assert.AreEqual(String.Empty, avl.Find(10));
+        }
+
+        /// <summary>
+        /// Дополнительные тесты
+        /// </summary>
+
+        [TestMethod()]
+        public void Test15()
+        {
+            AVL avl = new AVL();
+            avl.Add(4);
+            avl.Add(2);
+            avl.Add(5);
+            avl.Add(1);
+            avl.Add(3);
+            avl.Delete(5);
+            Assert.AreEqual("(1)(2)(3)(4)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test16()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(4);
+            avl.Add(3);
+            avl.Add(5);
+            avl.Delete(1);
+            Assert.AreEqual("(2)(3)(4)(5)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test17()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(4);
+            avl.Add(3);
+            avl.Delete(1);
+            Assert.AreEqual("(2)(3)(4)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test18()
+        {
+            AVL avl = new AVL();
+            avl.Add(3);
+            avl.Add(1);
+            avl.Add(4);
+            avl.Add(2);
+            avl.Delete(4);
+            Assert.AreEqual("(1)(2)(3)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test19()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            avl.Add(2);
+            avl.Add(4);
+            avl.Add(3);
+            avl.Delete(2);
+            Assert.AreEqual("(1)(3)(4)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test20()
+        {
+            AVL avl = new AVL();
+            avl.Add(3);
+            avl.Add(2);
+            avl.Add(4);
+            avl.Add(1);
+            avl.Delete(3);
+            Assert.AreEqual("(1)(2)(4)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test21()
+        {
+            AVL avl = new AVL();
+            avl.Add(3);
+            avl.Add(1);
+            avl.Add(4);
+            avl.Add(2);
+            avl.Delete(3);
+            Assert.AreEqual("(1)(2)(4)", avl.PrintTree());
+        }
+
+        [TestMethod()]
+        public void Test22()
+        {
+            AVL avl = new AVL();
+            avl.Add(1);
+            Assert.AreEqual(String.Empty, avl.Find(10));
+        }
+
+        [TestMethod()]
+        public void Test23()
+        {
+            AVL avl = new AVL();
+            avl.Add(10);
+            Assert.AreEqual(String.Empty, avl.Find(1));
+        }
+    }
+}
